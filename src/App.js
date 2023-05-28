07bf219c19e99e4c00304897622ef7b6177655b1
Ian Bui <thinkbui@gmail.com>
2023-05-28 16:48:30 -0700

    Added conditional show/hide to other sections

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [ mainTab, setMainTab ] = useState("")
+  const [ mainTab, setMainTab ] = useState("about-me")
 
   function switchTab(tab_name) {
     console.log(`Switching to ${tab_name}`);
@@ -35,44 +35,47 @@ function App() {
             </div>
           </article>
         }
-        <section id="code-examples">
-          <div className="heading">
-            <h2>Code Examples</h2>
-          </div>
-          <div className="content">
-            <a href="https://github.com/JacobDNelsonStone/live-music-search-ticketmaster-api" style={{backgroundImage: "url('assets/images/live-events-api.png')"}}>
-              <div className="example-code">
-                <h3>Live Events API (Group Project)</h3>
-              </div>
-            </a>
-            <a href="https://github.com/thinkbui/umn-edx-weather-dashboard" style={{backgroundImage: "url('assets/images/weather-dashboard.png')"}}>
-              <div className="example-code">
-                <h3>Weather Dashboard</h3>
-              </div>
-            </a>
-            <a href="https://github.com/thinkbui/umn-edx-work-day-scheduler" style={{backgroundImage: "url('assets/images/workday-scheduler.png')"}}>
-              <div className="example-code">
-                <h3>Workday Scheduler</h3>
-              </div>
-            </a>
-            <a href="https://github.com/thinkbui/umn-edx-timed-quiz" style={{backgroundImage: "url('assets/images/timed-quiz.png')"}}>
-              <div className="example-code">
-                <h3>Timed Quiz</h3>
-              </div>
-            </a>
-            <a href="https://github.com/thinkbui/umn-edx-password-generator" style={{backgroundImage: "url('assets/images/password-generator.png')"}}>
-              <div className="example-code">
-                <h3>Password Generator</h3>
-              </div>
-            </a>
-            <a href="https://github.com/thinkbui/umn-edx-module-1-challenge-code-refactor" style={{backgroundImage: "url('assets/images/refactor-challenge.png')"}}>
-              <div className="example-code">
-                <h3>Refactor Challenge</h3>
-              </div>
-            </a>
-          </div>
-      </section>
-        <section id="contact">
+        { mainTab == "code-examples" &&
+          <section id="code-examples">
+            <div className="heading">
+              <h2>Code Examples</h2>
+            </div>
+            <div className="content">
+              <a href="https://github.com/JacobDNelsonStone/live-music-search-ticketmaster-api" style={{backgroundImage: "url('assets/images/live-events-api.png')"}}>
+                <div className="example-code">
+                  <h3>Live Events API (Group Project)</h3>
+                </div>
+              </a>
+              <a href="https://github.com/thinkbui/umn-edx-weather-dashboard" style={{backgroundImage: "url('assets/images/weather-dashboard.png')"}}>
+                <div className="example-code">
+                  <h3>Weather Dashboard</h3>
+                </div>
+              </a>
+              <a href="https://github.com/thinkbui/umn-edx-work-day-scheduler" style={{backgroundImage: "url('assets/images/workday-scheduler.png')"}}>
+                <div className="example-code">
+                  <h3>Workday Scheduler</h3>
+                </div>
+              </a>
+              <a href="https://github.com/thinkbui/umn-edx-timed-quiz" style={{backgroundImage: "url('assets/images/timed-quiz.png')"}}>
+                <div className="example-code">
+                  <h3>Timed Quiz</h3>
+                </div>
+              </a>
+              <a href="https://github.com/thinkbui/umn-edx-password-generator" style={{backgroundImage: "url('assets/images/password-generator.png')"}}>
+                <div className="example-code">
+                  <h3>Password Generator</h3>
+                </div>
+              </a>
+              <a href="https://github.com/thinkbui/umn-edx-module-1-challenge-code-refactor" style={{backgroundImage: "url('assets/images/refactor-challenge.png')"}}>
+                <div className="example-code">
+                  <h3>Refactor Challenge</h3>
+                </div>
+              </a>
+            </div>
+          </section>
+        }
+        { mainTab == "contact" &&
+          <section id="contact">
           <div className="heading">
             <h2>Contact</h2>
           </div>
@@ -95,6 +98,7 @@ function App() {
             </div>
           </div>
         </section>
+      }
       </main>
 
       <footer>
