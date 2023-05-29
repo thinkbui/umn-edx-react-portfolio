645c6b8e486cd0c2e87ac5ddd2de30b6fa90a055
Ian Bui <thinkbui@gmail.com>
2023-05-28 23:48:32 -0700

    Updated visual references of code-examples

--- src/components/Main/CodeExamples.js
+++ src/components/Main/CodeExamples.js
@@ -4,7 +4,7 @@ function CodeExamples(props) {
   return (
     <section id="code-examples">
       <div className="heading">
-        <h2>Code Examples</h2>
+        <h2>Portfolio</h2>
       </div>
       <div className="content">
         {props.examples_list.map((example) => <Example example={example} />)}
