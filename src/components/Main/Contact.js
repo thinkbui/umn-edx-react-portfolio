ff67a0aad7095fc272da3aaaf94daf9a20fa2843
Ian Bui <thinkbui@gmail.com>
2023-05-29 16:29:02 -0700

    Added strucuture of form

--- src/components/Main/Contact.js
+++ src/components/Main/Contact.js
@@ -1,4 +1,37 @@
+import { useState } from "react"
+
 function Contact(props) {
+  const [ namePresence, setNamePresence ] = useState(true);
+  const [ emailPresence, setEmailPresence ] = useState(true);
+  const [ emailValid, setEmailValid ] = useState(true);
+
+  function name_presence() {
+    const name_value = document.getElementById("form_name").value
+    if (name_value) {
+      setNamePresence(true);
+    } else {
+      setNamePresence(false);
+    }
+  }
+
+  function email_presence() {
+    const name_value = document.getElementById("form_email").value
+    if (name_value) {
+      setEmailPresence(true);
+    } else {
+      setEmailPresence(false);
+    }
+  }
+
+  function email_validity() {
+    const name_value = document.getElementById("form_email").value
+    if (name_value) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
+  }
+
   return (
     <section id="contact">
       <div className="heading">
@@ -6,6 +39,18 @@ function Contact(props) {
       </div>
       <div>
         <div className="content">
+          <form>
+            <label>Name:</label>
+            <input type="text" id="form_name" name="name" onBlur={() => name_presence()}></input>
+            <span className="form_warn">{namePresence ? "" : "Required"}</span>
+
+            <label>Email:</label>
+            <input type="text" id="form_email" name="email" onBlur={() => email_presence()}></input>
+            <span className="form_warn">{emailPresence ? "" : "Required"}</span>
+
+            <label>Message:</label>
+            <input type=""></input>
+          </form>
         </div>
       </div>
     </section>
