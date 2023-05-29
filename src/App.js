2377a8891e8c5c3de1f50575e2d125fdac80f428
Ian Bui <thinkbui@gmail.com>
2023-05-29 13:27:52 -0700

    Added placeholder resume info

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
-import { headshot_src, header_name, tab_names, about_me_headshot_src, about_me_bio, examples_list, contact_options, resume_content, footer_text } from "./constants/Constants"
+import { headshot_src, header_name, tab_names, about_me_headshot_src, about_me_bio, examples_list, contact_options, resume_content, resume_link, footer_text } from "./constants/Constants"
 import './App.css';
 
 function App() {
@@ -28,6 +28,7 @@ function App() {
         about_me_bio={about_me_bio}
         examples_list={examples_list}
         resume_content={resume_content}
+        resume_link={resume_link}
       />
       <Footer
         contact_options={contact_options}
