1ee383e0303194b841979e311eea05523770863f
Ian Bui <thinkbui@gmail.com>
2023-05-30 01:29:58 -0700

    Condensed presence and validity checks

--- src/components/Main/Contact.js
+++ src/components/Main/Contact.js
@@ -7,34 +7,22 @@ function Contact(props) {
 
   function name_presence() {
     const name_value = document.getElementById("form_name").value
-    if (name_value) {
-      setNamePresence(true);
-    } else {
-      setNamePresence(false);
-    }
+    setNamePresence(!!name_value);
   }
 
   function email_check() {
-    email_presence()
-    email_validity()
+    email_presence();
+    email_validity();
   }
 
   function email_presence() {
     const email_value = document.getElementById("form_email").value
-    if (email_value) {
-      setEmailPresence(true);
-    } else {
-      setEmailPresence(false);
-    }
+    setEmailPresence(!!email_value);
   }
 
   function email_validity() {
     const email_value = document.getElementById("form_email").value
-    if (/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/i.test(email_value)) {
-      setEmailValid(true);
-    } else {
-      setEmailValid(false);
-    }
+    setEmailValid(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/i.test(email_value));
   }
 
   function formSubmitHandler(event) {
