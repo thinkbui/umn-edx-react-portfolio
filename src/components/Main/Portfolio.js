8f7c11ea553d1e6e5109a95ccb9a2e0f0ca8cfa9
Ian Bui <thinkbui@gmail.com>
2023-05-30 01:49:21 -0700

    Added keys to JSX

--- src/components/Main/Portfolio.js
+++ src/components/Main/Portfolio.js
@@ -7,7 +7,7 @@ function Portfolio(props) {
         <h2>{props.tab_name}</h2>
       </div>
       <div className="content">
-        {props.examples_list.map((example) => <CodeExample example={example} />)}
+        {props.examples_list.map((example) => <CodeExample key={example.key} example={example} />)}
       </div>
     </section>
   );
