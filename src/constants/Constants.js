51c81a243c82b182be38fc2ac4087b97c02d23c9
Ian Bui <thinkbui@gmail.com>
2023-08-08 00:28:37 -0500

    Added third paragraph

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -49,6 +49,10 @@ export const about_me_bio = [
     "key": "line1",
     "content": "I've recently completed a web development bootcamp through the University of Minnesota where the focus was on MERN including its underlying technologies (MongoDB, ExpressJS, ReactJS, NodeJS) and potential work with others such as Sequelize/MySQL.",
   },
+  {
+    "key": "line2",
+    "content": "Feedback from my previous jobs all say that I'm a very diligent developer with a great attention to detail creating code that is forward-thinking.  It is also very easy for me to get very passionate about each company's mission.",
+  },
 ];
 
 export const examples_list = [
