361c889c02002bfbd801ef974eb71b95abbe72d9
Ian Bui <thinkbui@gmail.com>
2023-08-07 20:41:47 -0500

    Removed JS for contact form

--- src/components/Main/Contact.js
+++ src/components/Main/Contact.js
@@ -1,42 +1,6 @@
-import { useState } from "react"
 import { tab_data } from "../../constants/Constants";
 
 function Contact(props) {
-  const [ namePresence, setNamePresence ] = useState(true);
-  const [ emailPresence, setEmailPresence ] = useState(true);
-  const [ emailValid, setEmailValid ] = useState(true);
-  const [ messagePresence, setMessagePresence ] = useState(true);
-
-  function name_presence() {
-    const name_value = document.getElementById("form_name").value
-    setNamePresence(!!name_value);
-  }
-
-  function email_check() {
-    email_presence();
-    email_validity();
-  }
-
-  function email_presence() {
-    const email_value = document.getElementById("form_email").value
-    setEmailPresence(!!email_value);
-  }
-
-  function email_validity() {
-    const email_value = document.getElementById("form_email").value
-    setEmailValid(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/i.test(email_value));
-  }
-
-  function message_presence() {
-    const message_value = document.getElementById("form_message").value
-    setMessagePresence(!!message_value);
-  }
-
-  function formSubmitHandler(event) {
-    event.preventDefault();
-    console.log("submit");
-  }
-
   return (
     <section id="contact">
       <div className="heading">
