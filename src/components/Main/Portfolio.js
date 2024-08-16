9889927c9c1389dc4694e84aef6d74129451b475
Ian Bui <thinkbui@gmail.com>
2024-08-16 04:48:28 -0500

    Added link to additional examples

--- src/components/Main/Portfolio.js
+++ src/components/Main/Portfolio.js
@@ -10,6 +10,7 @@ function Portfolio(props) {
       <div className="content">
         {examples_list.map((example) => <Project key={example.key} example={example} />)}
       </div>
+      <a href="http://thinkbui.github.io/" target="_blank" rel="noopener noreferrer">Additional Code Examples</a>
     </section>
   );
 }
